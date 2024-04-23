2a2ea4305f16287642ec428f03803b3cdd732ce9
Kim Janssens <kimjanssens21@gmail.com>
2024-04-23 18:52:30 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<main>
+		<pre>{{ form }}</pre>
+
+		<div>
+			<form novalidate @submit.prevent="handleSubmit">
+				<input v-model="form.name" type="text" />
+
+				<button type="submit">Submit</button>
+			</form>
+		</div>
+	</main>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+
+const form = reactive({});
+
+const handleSubmit = () => {
+	console.log("Form submitted");
+};
+</script>
